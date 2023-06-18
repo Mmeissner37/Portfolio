fa19802c0f3b9a83bbf644327789f55942b19134
Michaela Meissner <m.p.meissner85@gmail.com>
2023-06-18 07:13:30 -0400

    Added HandleSubmit to Handle Form Submission

--- frontend/src/pages/ContactMePage/ContactMePage.jsx
+++ frontend/src/pages/ContactMePage/ContactMePage.jsx
@@ -1,24 +1,47 @@
+import useCustomForm from '../../hooks/useCustomForm';
 import './ContactMePage.css'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
+let initialValues = {
+    name: "",
+    company: "",
+    email: "",
+}
 
 const ContactMePage = () => {
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues, newContact)
+    const navigate = useNavigate();
+
+    async function newContact() {
+        try {
+            let response = await axios.post('', formData, {})
+            navigate('/')
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+
+
     return ( 
-        <div className="container">
-            <h2 className='header'>Contact Me</h2><br></br>
-            <form className="form">
-                <label>Name: 
-                    <input type='text' /><br></br>
-                </label>
-                <label>Company: 
-                    <input type='text' /><br></br>
-                </label>
-                <label>Email Address: 
-                    <input type='text' /><br></br>
-                </label>
-                <div className='contact-button'>
-                    <button>Submit</button>
-                </div>
-            </form>
+        <div className='contact-page'>
+            <div className="container">
+                <h2 className='header'>Contact Me</h2><br></br>
+                <form className="form" onSubmit={handleSubmit}>
+                    <label className='contact-form'>Name: {' '}
+                        <input type='text' name='name' value={formData.name} onChange={handleInputChange} /><br></br>
+                    </label>
+                    <label className='contact-form'>Company: {' '}
+                        <input type='text' name='company' value={formData.company} onChange={handleInputChange} /><br></br>
+                    </label>
+                    <label className='contact-form'>Email Address: {' '}
+                        <input type='text' name='email' value={formData.email} onChange={handleInputChange}/><br></br>
+                    </label>
+                    <div className='contact-button'>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
      );
 }
