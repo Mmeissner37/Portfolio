bd29b89d8df511ae3563f2f8c0fb302e3ef052f4
Michaela Meissner <m.p.meissner85@gmail.com>
2023-08-17 20:41:23 -0400

    Added Padding to AboutMe Page for Mobile Users

--- frontend/src/components/Certificates.jsx
+++ frontend/src/components/Certificates.jsx
@@ -3,20 +3,19 @@ import FCC from "../assets/FCC_ResponsiveWebDesign.png"
 
 const Certficates = () => {
   return (
-    <div className="certificates">
+    <div>
       <div className="journey-header">
         <h2>Current Certificates</h2>
         <br></br>
       </div>
-      <div class="certfiicates">
+      <div className="certificates">
         <div className="image-link">
           <img src={DCC} height="200" width="275" alt="DCC Certficate" />
         </div>
         <div className="image-link">
-          <h4>*New 8/2023*</h4>
           <img src={FCC} height="200" width="275" alt="FCC Certficiate" />
         </div>
-      </div>
+        </div>
     </div>
   );
 };
