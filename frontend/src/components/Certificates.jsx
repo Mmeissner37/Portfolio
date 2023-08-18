747206efbcabab32125242e19a2e5516b858ad57
Michaela Meissner <m.p.meissner85@gmail.com>
2023-08-17 20:30:00 -0400

    Added FCC Certificate to AboutMe Section

--- frontend/src/components/Certificates.jsx
+++ frontend/src/components/Certificates.jsx
@@ -1,14 +1,21 @@
 import DCC from "../assets/DCC_Certificate.png";
+import FCC from "../assets/FCC_ResponsiveWebDesign.png"
 
 const Certficates = () => {
   return (
-    <div className="certicates">
+    <div className="certificates">
       <div className="journey-header">
         <h2>Current Certificates</h2>
         <br></br>
       </div>
-      <div className="image-link">
-        <img src={DCC} height="200" width="275" alt="DCC Certficate" />
+      <div class="certfiicates">
+        <div className="image-link">
+          <img src={DCC} height="200" width="275" alt="DCC Certficate" />
+        </div>
+        <div className="image-link">
+          <h4>*New 8/2023*</h4>
+          <img src={FCC} height="200" width="275" alt="FCC Certficiate" />
+        </div>
       </div>
     </div>
   );
